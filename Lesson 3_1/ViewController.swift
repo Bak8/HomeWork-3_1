109e38607b1957e70d47ec35a155e98cd7fc33f4
Bak8 <ataibegaliev@gmail.com>
2022-11-21 13:55:52 +0600

    Ver 1

--- Lesson 3_1/ViewController.swift
+++ Lesson 3_1/ViewController.swift
@@ -9,6 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+//    @IBOutlet weak var labelResult: UILabel!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    
+    @IBAction func button(_ sender: Any) {
+
+        
+        if loginTextField.text!.count > 3 {
+            result.text = ("Успешно")
+            result.textColor = UIColor.green
+            
+            if passwordTextField.text!.count > 3 {
+                result.text = ("Успешно")
+                result.textColor = UIColor.green
+            }else{
+                result.text = ("Введите больше 3х символов")
+                result.textColor = UIColor.red
+            }
+            
+        }else{
+            result.text = ("Введите больше 3х символов")
+            result.textColor = UIColor.red
+        }
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
